8328a3c6b742ee59b4aab5c07847f0d28211918f
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-11-23 00:13:01 +0100

    MapScreen storage use

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MapComponent from '../components/MapComponent';
-
+import useStore from '../hooks/Store';
 export default function MapScreen() {
+    const {location , setLocation , setZoom} = useStore();
     return (
         <View>
-           
+           <MapComponent {...location} />
         </View>
     )
 }
